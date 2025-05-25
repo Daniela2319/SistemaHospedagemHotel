4c1374c07fec332f3e205f016337f8708e3081ff
Daniela2319 <frankvt04@gmail.com>
2025-05-25 19:49:38 -0300

    criada as classes

---
+++ App-hotel/App-hotel/Models/Reserva.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_hotel.Models
+{
+    internal class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva(List<Pessoa> hospedes, Suite suite, int diasReservados)
+        {
+            Hospedes = hospedes;
+            Suite = suite;
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospede(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count > Suite.Capacidade)
+            {
+                throw new Exception("Número de hóspedes excede a capacidade da suíte.");
+            }
+            Hospedes = hospedes;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int obterQuantidadeHospedes()
+        {
+            if (Hospedes == null || !Hospedes.Any())
+            {
+                throw new Exception("Nenhum hóspede cadastrado.");
+            }
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.9m;
+            }
+
+            return valor;
+        }
+    }
+}
